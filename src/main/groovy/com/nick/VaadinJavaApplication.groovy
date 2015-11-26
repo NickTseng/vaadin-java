ab7563c0a7638167d6c8774e692295998389c60e
nick.tseng <z77819@gmail.com>
2015-11-26 13:48:44 +0800

    project init

---
+++ src/main/groovy/com/nick/VaadinJavaApplication.groovy
@@ -0,0 +1,12 @@
+package com.nick
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class VaadinJavaApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run VaadinJavaApplication, args
+    }
+}
